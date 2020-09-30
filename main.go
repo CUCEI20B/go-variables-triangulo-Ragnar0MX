17e6b1d3628481bd23b9af302f774d6a31014dbb
Moises Rafael <rafael.plascencia@alumnos.udg.mx>
2020-09-30 10:31:10 -0500

    prueba01

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var a, b, c float64
+	a = (3 * 4) / 2
+	b = (100 * 200) / 2
+	c = (1000 * 1000) / 2
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
 }
